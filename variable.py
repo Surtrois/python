10e62691f63c7b1de3552610ac36557a0aacd32f
surtrois <maxime.62420@hotmail.fr>
2022-11-29 11:52:33 +0100

    J'ai rajouté deux ligne deux ligne de code et  expliqué comment ça marche

--- variable.py
+++ variable.py
@@ -1,3 +1,12 @@
+#l'operateur permet d'affecter une valeur dans une variable 
+
+my_number1 = 123
+my_number2 = -784
+
+print(my_number1)
+print(my_number2)
+
+
 my_text1="ceci est une chaine de caractere"
 my_text2="ceci est aussi ça"
 
