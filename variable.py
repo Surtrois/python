545cc078cfaf88fb8520482880af2ff389583916
surtrois <maxime.62420@hotmail.fr>
2022-12-05 11:16:04 +0100

    oui

--- variable.py
+++ variable.py
@@ -12,3 +12,76 @@ my_text2="ceci est aussi ça"
 
 print(my_text1)
 print(my_text2)
+
+my_boolean1 = True
+my_boolean2 = False
+
+print(my_boolean1)
+print(my_boolean2)
+
+bite = "crayon"#l'operateur permet d'affecter une valeur dans une variable 
+
+my_number1 = 123
+my_number2 = -784
+
+print(my_number1)
+print(my_number2)
+
+
+my_text1="ceci est une chaine de caractere"
+my_text2="ceci est aussi ça"
+
+print(my_text1)
+print(my_text2)
+
+my_boolean1 = True
+my_boolean2 = False
+
+print(my_boolean1)
+print(my_boolean2)
+
+bite = "crayon"
+print(bite)#l'operateur permet d'affecter une valeur dans une variable 
+
+my_number1 = 123
+my_number2 = -784
+
+print(my_number1)
+print(my_number2)
+
+
+my_text1="ceci est une chaine de caractere"
+my_text2="ceci est aussi ça"
+
+print(my_text1)
+print(my_text2)
+
+my_boolean1 = True
+my_boolean2 = False
+
+print(my_boolean1)
+print(my_boolean2)
+
+bite = "crayon"#l'operateur permet d'affecter une valeur dans une variable 
+
+my_number1 = 123
+my_number2 = -784
+
+print(my_number1)
+print(my_number2)
+
+
+my_text1="ceci est une chaine de caractere"
+my_text2="ceci est aussi ça"
+
+print(my_text1)
+print(my_text2)
+
+my_boolean1 = True
+my_boolean2 = False
+
+print(my_boolean1)
+print(my_boolean2)
+
+
+
