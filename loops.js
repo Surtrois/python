b06a8b19ebfc669da170a5f741d61f2793dd33b4
surtrois <maxime.62420@hotmail.fr>
2022-12-19 13:35:08 +0100

    loops

---
+++ loops.js
@@ -0,0 +1,4 @@
+//i++ est equivalent a i+= 1
+for (let i = 0; i < 10; i++) {
+    console.log(`i = ${i}`)
+}
--- loops.py
+++ loops.py
@@ -20,3 +20,28 @@ while counter == 1:
     print(f"{counter}")
     counter += 1
 #rajoute + 1 a chaque fois 
+
+counter = 0
+
+while counter != 1:
+    print(f"{counter}")
+    counter += 1
+
+#!= veut dire different de 
+
+for counter in range(0, 10):
+    print(f"{counter =}")
+
+fruits = ['abricot', 'pomme', 'cerise']
+result = 'ananas' in fruits
+print(result)
+result = 'pomme' in fruits
+print(result)
+
+#permet de verifier si un truc est dans une liste 
+for fruit in fruits:
+    print(fruit)
+
+for i, fruit in enumerate(fruits):
+    print(f'{i}: {fruit}')
+#enumere ce qu'il y a dans la liste
