ce5edeaf53dac90f2693b7668c07037182e614f5
surtrois <maxime.62420@hotmail.fr>
2022-12-19 16:02:21 +0100

    m

--- exercices-06/exercice-06-15.py
+++ exercices-06/exercice-06-15.py
@@ -5,6 +5,12 @@
 my_list = ['Lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipiscing', 'elit']
 
 # réponse 6.15
-
+index = 0
+longueur = 0
 for i in range(len(my_list)):
+    if len(my_list[i]) > longueur:
+        longueur= len(my_list[i])
+        index = i
     
+print(index, my_list[index], longueur)
+
