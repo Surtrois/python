44bbb0c3ea6cb4055b2dd6b9271ccf39b5a15a33
surtrois <maxime.62420@hotmail.fr>
2022-12-19 13:34:20 +0100

    loops

--- loops.py
+++ loops.py
@@ -2,14 +2,21 @@ import random
 kk = random.randint(0,10)
 
 #while kk == 5:
-    print('ce message ne sera pas affiché')
+#   print('ce message ne sera pas affiché')
 
 #while kk == 0:
-    print('ce message sera affiché en boucle')
+#  print('ce message sera affiché en boucle')
 
-else: print('Perdu')
+#else: print('Perdu')
 
 counter = 10
 while counter:
     print(f"{counter}")
     counter -= 1
+
+counter = 1
+
+while counter == 1:
+    print(f"{counter}")
+    counter += 1
+#rajoute + 1 a chaque fois 
